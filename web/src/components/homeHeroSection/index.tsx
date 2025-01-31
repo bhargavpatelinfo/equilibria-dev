6bdfe7fe8037a7995d6379e7a63c96222ae9eafd
bhargav <bhargavpatel.info@gmail.com>
2025-01-31 17:08:24 +0530

    hom hero section

--- web/src/components/homeHeroSection/index.tsx
+++ web/src/components/homeHeroSection/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import { HomeHeroSectionType } from "../../../lib/sanity/types";
+import RichText from "../global/richText";
 
 const HomeHeroSection: React.FC<HomeHeroSectionType> = (block) => {
   const { id, title, description } = block || {};
   return (
-    <section id={id} className="bg-theme-lightGray pt-[211px] pb-[228px]">
-      {/* {title?.custom_rich_text && (
+    <section id={id} className="bg-[red] pt-[211px] pb-[228px]">
+      {title?.custom_rich_text && (
         <div className="[&>p]:text-theme-slateGray [&>p]:text-62px [&>p]:font-semibold [&>p]:text-center [&>p]:leading-1.3">
           <RichText block={title} />
         </div>
@@ -15,7 +16,7 @@ const HomeHeroSection: React.FC<HomeHeroSectionType> = (block) => {
         <div className="[&>p]:text-theme-slateGray [&>p]:text-base [&>p]:font-normal [&>p]:text-center">
           <RichText block={description} />
         </div>
-      )} */}
+      )}
     </section>
   );
 };
