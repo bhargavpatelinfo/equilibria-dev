37be1c0e8ea19fa185cb418ca64f688e5312fc00
bhargav <bhargavpatel.info@gmail.com>
2025-02-01 15:40:26 +0530

    Latest design Leaders Section

--- web/src/components/leadersSection/index.tsx
+++ web/src/components/leadersSection/index.tsx
@@ -3,52 +3,72 @@ import { LeadersSectionType } from "../../../lib/sanity/types";
 import RichText from "../global/richText";
 import Button from "../global/button";
 import Image from "../global/image";
+import dynamic from "next/dynamic";
+const Masonry = dynamic(() => import('react-layout-masonry'), { ssr: false });
 
 const LeadersSection: React.FC<LeadersSectionType> = (block) => {
   const { id, title, buttons, description, leaders } = block || {};
   return (
-    <section id={id} className="bg-[red] pt-[211px] pb-[228px]">
-      {title?.custom_rich_text && (
-        <div className="[&>p]:text-theme-slateGray [&>p]:text-62px [&>p]:font-semibold [&>p]:text-center [&>p]:leading-1.3">
-          <RichText block={title} />
-        </div>
-      )}
-      {description?.custom_rich_text && (
-        <div className="[&>p]:text-theme-slateGray [&>p]:text-62px [&>p]:font-semibold [&>p]:text-center [&>p]:leading-1.3">
-          <RichText block={description} />
-        </div>
-      )}
-      {buttons?.length > 0 && (
-        <div>
-          {buttons?.map((item, index) => {
-            return (
-              <div key={index}>
-                <Button block={item} />
-              </div>
-            );
-          })}
-        </div>
-      )}
-      {leaders?.length > 0 && (
-        <div>
-          {leaders?.map((item, index) => {
-            const { bio, image, name, slug } = item || {};
-            return (
-              <div key={index}>
-                <div>
-                  {name && <div>{name}</div>}
-                  {bio && <div>{bio}</div>}
-                  {image && (
-                    <div>
-                      <Image src={image} alt={image?.alt} />
-                    </div>
-                  )}
+    <section id={id} className="mb-[120px] lg:mb-[200px] xl:mb-[264px]">
+      <div className="container">
+        <div className="flex flex-col lg:flex-row justify-between gap-16 lg:gap-10">
+          <div className="max-w-full lg:max-w-[604px] w-full flex flex-col gap-8 em:gap-12">
+            <div className="flex flex-col gap-6">
+              {title?.custom_rich_text && (
+                <div className="[&>*]:text-[36px] em:[&>*]:text-[46px] sm:[&>*]:text-[50px] lg:[&>*]:text-[64px] [&>*]:text-darkPurple [&>*]:tracking-[-1.28px] [&>*]:font-semibold [&>*]:font-Kulim [&>*]:leading-[110%]">
+                  <RichText block={title} />
                 </div>
+              )}
+              {description?.custom_rich_text && (
+                <div className="[&>*]:text-lg em:[&>*]:text-xl [&>*]:font-medium [&>*]:tracking-[-0.1px] [&>*]:text-darkPurple">
+                  <RichText block={description} />
+                </div>
+              )}
+            </div>
+            {buttons?.length > 0 && (
+              <div className="flex flex-col em:flex-row gap-4">
+                {buttons?.map((item, index) => {
+                  return (
+                    <div key={index}>
+                      <Button block={item} />
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
+            )}
+          </div>
+          <div className="max-w-full lg:max-w-[800px] w-full">
+            {leaders?.length > 0 && (
+              <Masonry
+              columns={{ 320: 1, 640: 2 }}
+              gap={32}
+              columnProps={{
+                className: '',
+              }}
+              >
+                {leaders?.map((item, index) => {
+                  const { bio, image, name, slug } = item || {};
+                  return (
+                    <div key={index} className="p-8 pb-0 bg-[#FFE8F4] rounded-[32px]">
+
+                      <div className="flex flex-col gap-4">
+                        {name && <h3 className="text-[32px] xl:text-[40px] font-semibold tracking-[-0.8px] font-Kulim text-darkPink leading-[110%] opacity-50">{name}</h3>}
+                        {bio && <p className="text-xl font-semibold tracking-[-0.1px] leading-[150%] text-darkPurple">{bio}</p>}
+                      </div>
+                      {image && (
+                        <div className="mt-[50px]">
+                          <Image src={image} alt={image?.alt} className="w-auto h-auto"/>
+                        </div>
+                      )}
+
+                    </div>
+                  );
+                })}
+              </Masonry>
+            )}
+          </div>
         </div>
-      )}
+      </div>
     </section>
   );
 };
