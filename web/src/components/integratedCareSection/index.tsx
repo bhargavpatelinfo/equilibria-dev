8f1a91a7839272cf70541aa57c2a5fcd4ffbaf6e
bhargav <bhargavpatel.info@gmail.com>
2025-02-12 14:38:20 +0530

    update code

--- web/src/components/integratedCareSection/index.tsx
+++ web/src/components/integratedCareSection/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { IntegratedCareSectionType } from '../../../lib/sanity/types';
 import RichText from '../global/richText';
+import { groupItems } from '../../../lib/utils/helperFunctions';
 
 const IntegratedCareSection: React.FC<IntegratedCareSectionType> = (block) => {
     const { id, title, integratedCares } = block || {};
+    const groupIntegratedCares = groupItems(integratedCares, 3)
     return (
         <section id={id} className="mb-[80px] lg:mb-[200px] xl:mb-[264px]">
             <div className="container">
@@ -16,30 +18,47 @@ const IntegratedCareSection: React.FC<IntegratedCareSectionType> = (block) => {
                                 </div>
                             )}
                         </div>
-                        {integratedCares?.length > 0 && (
-                            <div className="flex flex-col em:flex-row gap-4">
-                                {integratedCares?.map((item, index) => {
-                                    const { features, title } = item || {}
+                        {groupIntegratedCares?.length > 0 && (
+                            <div className="flex "> 
+                                {groupIntegratedCares?.map((groupItem, groupIndex) => {
                                     return (
-                                        <div key={index}>
-                                            {title &&
-                                                <div>
-                                                    {title}
-                                                </div>
-                                            }
-                                            {features?.length > 0 &&
-                                            <div>
-                                                {features?.map((item,index)=>{
-                                                    return(
-                                                        <div key={index}>
-                                                            {item}
-                                                        </div>
-                                                    )
-                                                })}
-                                            </div>
-                                            }
+                                        <div key={groupIndex}className="grid grid-cols-3" >
+                                            {groupItem?.map((item, itemIndex) => {
+                                                const { features, title } = item || {}
+                                                const updatedCount = itemIndex + 1;
+                                                const formattedIndex = updatedCount < 10 ? `0${updatedCount}` : updatedCount;
+                                                return (
+                                                    <div key={itemIndex} >
+                                                        {formattedIndex &&
+                                                            <div className="">
+                                                                {formattedIndex &&
+                                                                    <div className="w-12 h-12 flex items-center justify-center rounded-full border border-[#C43D6A] text-darkPurple font-bold">
+                                                                        {formattedIndex}
+                                                                    </div>
+                                                                }
+                                                                {itemIndex < groupItem?.length - 1 && (
+                                                                    <div className="w-16 border-t border-dashed border-[#C43D6A] mt-6" />
+                                                                )}
+                                                            </div>
+                                                        }
+                                                        {title &&
+                                                            <h2 className="">{title}
+                                                            </h2>
+                                                        }
+                                                        {features?.length > 0 && (
+                                                            <div className="">
+                                                                {features?.map((feature, featureIndex) => (
+                                                                    <div key={featureIndex} className="">
+                                                                        {feature}
+                                                                    </div>
+                                                                ))}
+                                                            </div>
+                                                        )}
+                                                    </div>
+                                                );
+                                            })}
                                         </div>
-                                    );
+                                    )
                                 })}
                             </div>
                         )}
