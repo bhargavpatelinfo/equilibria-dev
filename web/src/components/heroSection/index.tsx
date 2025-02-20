085596da1e89393fdef82906f3fe304b79287a8f
bhargav <bhargavpatel.info@gmail.com>
2025-02-20 18:22:00 +0530

    hero section design done
    hero section design done

--- web/src/components/heroSection/index.tsx
+++ web/src/components/heroSection/index.tsx
@@ -1,74 +1,79 @@
-import React from 'react'
-import { HeroSectionType } from '../../../lib/sanity/types';
-import RichText from '../global/richText';
-import Image from '../global/image';
-import Button from '../global/button';
-import GreenTrueIcon from '../commons/icons/greenTrueIcon';
+import React from "react";
+import { HeroSectionType } from "../../../lib/sanity/types";
+import RichText from "../global/richText";
+import Image from "../global/image";
+import Button from "../global/button";
+import GreenTrueIcon from "../commons/icons/greenTrueIcon";
 
 const HeroSection: React.FC<HeroSectionType> = (block) => {
-    const { id, description, buttons, image, title, features } = block || {};
-    return (
-        <section id={id} className='mt-[120px] sm:mt-[150px] mb-[50px] lg:mb-[100px] xl:mb-[160px]'>
-            <div className='container'>
-                <div className='flex flex-col lg:flex-row items-center gap-10 justify-between'>
-                    <div className='lg:basis-[60%] xl:basis-1/2 flex flex-col gap-8 sm:gap-12'>
-                        <div className='flex flex-col gap-5 sm:gap-8'>
-                            {title?.custom_rich_text &&
-                                <div className='[&>*]:text-darkPink [&>*]:text-36 em:[&>*]:text-46 sm:[&>*]:text-50 lg:[&>*]:text-64 [&>*]:font-Kulim [&>*]:font-semibold [&>*]:leading-[120%] [&>*]:tracking-[-1.28px]'>
-                                    <RichText block={title} />
-                                </div>
-                            }
-                            {description?.custom_rich_text &&
-                                <div className='[&>*]:text-lg sm:[&>*]:text-xl [&>*]:text-maroon [&>*]:tracking-[-0.1px] [&>*]:leading-[150%]'>
-                                    <RichText block={description} />
-                                </div>
-                            }
-                        </div>
-
-                        {features?.length > 0 &&
-                            <div className='pt-8 sm:pt-12 border-t border-[#DDDDDD] grid grid-cols-1 sm:grid-cols-2 gap-6'>
-                                {features?.map((item, index) => {
-                                    return (
-                                        <div key={index} className='flex gap-4'>
-                                            <div>
-                                                <GreenTrueIcon />
-                                            </div>
-                                            <div className='text-lg xl:text-xl text-maroon font-medium tracking-[-0.1px]'>
-                                                {item}
-                                            </div>
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        }
-                        {buttons?.length > 0 &&
-                            <div className='flex flex-wrap gap-4 items-center'>
-                                {buttons?.map((item, index) => {
-                                    return (
-                                        item?.label &&
-                                        <div key={index} className='w-full em:w-auto'>
-                                            <Button block={item} className='!px-6 lg:!px-4 xl:!px-6 1xl:!px-10 !text-base xl:!text-lg ' />
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        }
-                    </div>
-                    {image &&
-                        <div className='lg:basis-[35%] xl:basis-1/2 mx-auto'>
-                            <div className='aspect-square'>
-                                <Image
-                                    alt={image?.alt}
-                                    src={image}
-                                    className='w-full h-[280px] sm:h-[350px] md:h-[500px] lg:h-full object-cover'
-                                />
-                            </div>
-                        </div>
-                    }
+  const { id, description, buttons, image, title, features } = block || {};
+  return (
+    <section
+      id={id}
+      className="mt-[120px] sm:mt-[150px] mb-[50px] lg:mb-[100px] xl:mb-[160px]"
+    >
+      <div className="container">
+        <div className="flex flex-col lg:flex-row items-center gap-10 justify-between">
+          <div className="lg:basis-[60%] xl:basis-1/2 flex flex-col gap-8 sm:gap-12">
+            <div className="flex flex-col gap-5 sm:gap-8">
+              {title?.custom_rich_text && (
+                <div className="[&>*]:text-darkPink [&>*]:text-36 em:[&>*]:text-46 sm:[&>*]:text-50 lg:[&>*]:text-64 [&>*]:font-Kulim [&>*]:font-semibold [&>*]:leading-[120%] [&>*]:tracking-[-1.28px]">
+                  <RichText block={title} />
                 </div>
+              )}
+              {description?.custom_rich_text && (
+                <div className="[&>*]:text-lg sm:[&>*]:text-xl [&>*]:text-maroon [&>*]:tracking-[-0.1px] [&>*]:leading-[150%]">
+                  <RichText block={description} />
+                </div>
+              )}
             </div>
-        </section>
-    )
-}
 
-export default HeroSection
+            {features?.length > 0 && (
+              <div className="pt-8 sm:pt-12 border-t border-[#DDDDDD] grid grid-cols-1 sm:grid-cols-2 gap-6">
+                {features?.map((item, index) => {
+                  return (
+                    <div key={index} className="flex gap-4">
+                      <div>
+                        <GreenTrueIcon />
+                      </div>
+                      <div className="text-lg xl:text-xl text-maroon font-medium tracking-[-0.1px]">
+                        {item}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+            {buttons?.length > 0 && (
+              <div className="flex flex-wrap gap-4 items-center">
+                {buttons?.map((item, index) => {
+                  return (
+                    item?.label && (
+                      <div key={index} className="w-full em:w-auto">
+                        <Button
+                          block={item}
+                          className="!px-6 lg:!px-4 xl:!px-6 1xl:!px-10 !text-base xl:!text-lg "
+                        />
+                      </div>
+                    )
+                  );
+                })}
+              </div>
+            )}
+          </div>
+          {image && (
+            <div className="">
+              <Image
+                alt={image?.alt}
+                src={image}
+                className="w-full max-w-full md:max-w-[588px] h-[280px] sm:h-[350px] md:h-[661px] lg:h-full object-cover"
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- web/src/components/homeHeroSection/index.tsx
+++ web/src/components/homeHeroSection/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import { HomeHeroSectionType } from "../../../lib/sanity/types";
 import RichText from "../global/richText";
-import TextTransition, { presets } from 'react-text-transition';
+import TextTransition, { presets } from "react-text-transition";
 import Image from "../global/image";
 
-
 const HomeHeroSection: React.FC<HomeHeroSectionType> = (block) => {
-  const { id, description, suffix, prefix, rotatingSubtitles,image } = block || {};
+  const { id, description, suffix, prefix, rotatingSubtitles, image } =
+    block || {};
   const [index, setIndex] = React.useState(0);
 
   React.useEffect(() => {
@@ -19,53 +19,43 @@ const HomeHeroSection: React.FC<HomeHeroSectionType> = (block) => {
   }, []);
 
   return (
-    <section id={id} className="mb-[50px] sm:mb-[64px] lg:mb-[173px] mt-[120px] em:mt-[140px] lg:mt-[200px] xl:mt-[230px]">
+    <section id={id} className="mt-[60px]">
       <div className="container">
-        <div className="flex flex-col lg:flex-row items-center gap-10 justify-between">
-        <div>
-          <div>
-            <h1 className="[&>*]:text-maroon [&>*]:text-36 em:[&>*]:text-46 sm:[&>*]:text-50 lg:[&>*]:text-64 [&>*]:font-Kulim [&>*]:font-semibold [&>*]:leading-[120%] [&>*]:tracking-[-1.28px]">
-              <div className="flex flex-col sm:flex-row sm:items-center  gap-1 sm:gap-6 overflow-hidden">
-                {prefix &&
-                  <div>
-                    {prefix}
-                  </div>
-                }
-                {rotatingSubtitles?.length > 0 && (
-                  <div>
-                    <TextTransition
-                      springConfig={presets.wobbly}
-                      className=" text-darkPink  !h-auto font-semibold !lowercase"
-                    >
-                      {rotatingSubtitles?.[index % rotatingSubtitles?.length]}
-                    </TextTransition>
-                  </div>
-                )}
-              </div>
-              {suffix &&
-                <div className="max-w-[850px] w-full">
-                  {suffix}
+        <div className="flex flex-col lg:flex-row items-center gap-4 justify-between">
+          <div className="py-6 w-full lg:max-w-[820px]">
+            <div className="">
+              <h1 className="[&>*]:text-maroon [&>*]:text-36 em:[&>*]:text-46 sm:[&>*]:text-50 xl:[&>*]:text-64 [&>*]:font-Kulim [&>*]:font-semibold [&>*]:leading-[120%] [&>*]:tracking-[-1.28px]">
+                <div className="flex flex-col sm:flex-row sm:items-center  gap-1 sm:gap-6 overflow-hidden">
+                  {prefix && <div>{prefix}</div>}
+                  {rotatingSubtitles?.length > 0 && (
+                    <div>
+                      <TextTransition
+                        springConfig={presets.wobbly}
+                        className=" text-darkPink  !h-auto font-semibold !lowercase"
+                      >
+                        {rotatingSubtitles?.[index % rotatingSubtitles?.length]}
+                      </TextTransition>
+                    </div>
+                  )}
                 </div>
-              }
-            </h1>
-          </div>
-          {description?.custom_rich_text && (
-            <div className="max-w-[850px] w-full mt-6 em:mt-8 [&>*]:text-lg sm:[&>*]:text-xl [&>*]:tracking-[0.5%] [&>*]:text-maroon">
-              <RichText block={description} />
+                {suffix && <div className="max-w-[850px] w-full">{suffix}</div>}
+              </h1>
             </div>
-          )}
-        </div>
-        {image &&
-          <div className='lg:basis-[35%] xl:basis-1/2 mx-auto'>
-            <div className='aspect-square'>
+            {description?.custom_rich_text && (
+              <div className=" lg:max-w-[850px] w-full mt-6 em:mt-8 [&>*]:text-lg sm:[&>*]:text-xl [&>*]:tracking-[0.5%] [&>*]:text-maroon">
+                <RichText block={description} />
+              </div>
+            )}
+          </div>
+          {image && (
+            <div className="w-full max-w-full md:max-w-[537px] aspect-[537/671]">
               <Image
                 alt={image?.alt}
                 src={image}
-                className='w-full h-[280px] sm:h-[350px] md:h-[500px] lg:h-full object-cover'
+                className="w-full  h-full object-cover"
               />
             </div>
-          </div>
-        }
+          )}
         </div>
       </div>
     </section>
